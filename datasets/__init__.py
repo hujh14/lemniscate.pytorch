5d64e48684d60804f23e39037e46bc87436fee54
Jeffrey Hu <hujh14@gmail.com>
2019-05-29 14:11:49 -0400

    places

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,6 +1,7 @@
 from .folder import ImageFolderInstance
 from .cifar import CIFAR10Instance, CIFAR100Instance
 from .mnist import MNISTInstance
+from .coco import COCODataset
 
-__all__ = ('ImageFolderInstance', 'MNISTInstance', 'CIFAR10Instance', 'CIFAR100Instance')
+__all__ = ('ImageFolderInstance', 'MNISTInstance', 'CIFAR10Instance', 'CIFAR100Instance', 'COCODataset')
 
